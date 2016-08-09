45205da893b2ece62cb6b148ce8580111ab4531f
Wichuda Waelveerakup <falom.wiz@gmail.com>
2016-08-09 16:25:05 +0700

    CMI_Template version1
    Tester_.php

---
+++ dompdf/tester_.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+body {
+  background: rgb(204,204,204); 
+}
+page {
+  background: white;
+  display: block;
+  margin: 0 0 0 0;
+  box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
+}
+page[size="A4"] {  
+  width: 21cm;
+  height: 29.7cm; 
+}
+page[size="A4"][layout="portrait"] {
+  width: 29.7cm;
+  height: 21cm;  
+}
+page[size="A3"] {
+  width: 29.7cm;
+  height: 42cm;
+}
+page[size="A3"][layout="portrait"] {
+  width: 42cm;
+  height: 29.7cm;  
+}
+page[size="A5"] {
+  width: 14.8cm;
+  height: 21cm;
+}
+page[size="A5"][layout="portrait"] {
+  width: 21cm;
+  height: 14.8cm;  
+}
+@media print {
+  body, page {
+    margin: 0 0 0 0;
+    box-shadow: 0;
+  }
+}
+table{
+	border-collapse: collapse;
+	width:100%; 
+}
+table, th, td {
+    border: 1px solid black;
+}
+div {
+    font-size:x-small;
+}
+    </style>
+  </head>
+  <body>
+ <page size="A4">
+ <table height="10%">
+  	<tr>
+  		<td></td>
+  		<td></td>
+  		<td></td>
+  	</tr>
+  </table>
+  <table height="60%">
+  	<tr><th colspan="6"><div>
+  		สำเนาตารางกรมธรรม์ประกันภัยคุ้มครองผู้ประสยภัยจากรถ/ใบเสร็จ/ใบกำกับภาษี<br>
+  		THE SCHEDULE/RECEIPT/TAX INVOICE COPY
+  	</div></th></tr>
+	<tr><td><div>row2</div></td></tr>
+	<tr><td><div>row3</div></td></tr>
+	<tr><td><div>row4</div></td></tr>
+	<tr><td><div>row5</div></td></tr>
+	<tr>
+	<td><div>row6.1</div></td>
+	<td><div>row6.2</div></td>
+	<td><div>row6.3</div></td>
+	<td><div>row6.4</div></td>
+	<td><div>row6.5</div></td>
+	<td><div>row6.6</div></td>
+
+	</tr>
+	<tr>
+	<td><div>row7.1</div></td>
+	<td><div>row7.2</div></td>
+	<td><div>row7.3</div></td>
+	<td><div>row7.4</div></td>
+	<td><div>row7.5</div></td>
+	<td><div>row7.6</div></td>
+	</tr>
+	<tr><td><div>row8</div></td></tr>
+	<tr><td><div>row9</div></td></tr>
+	<tr><td><div>row10</div></td></tr>
+	<tr>
+		<td><div>row11.1</div></td>
+		<td><div>row11.2</div></td>
+		<td><div>row11.3</div></td>
+		<td><div>row11.4</div></td>
+		<td><div>row11.5</div></td>
+		<td><div>row11.6</div></td>
+	</tr>
+	<tr>
+		<td><div>row12.1</div></td>
+		<td><div>row12.2</div></td>
+		<td><div>row12.3</div></td>
+		<td><div>row12.4</div></td>
+		<td><div>row12.5</div></td>
+		<td><div>row12.6</div></td>
+	</tr>
+	<tr><td><div>row13</div></td></tr>
+	<tr><td><div>row14</div></td></tr>
+	<tr><td><div>row15</div></td></tr>
+	<tr><td><div>row16</div></td></tr>
+	<tr><td><div>row17</div></td></tr>
+
+
+  </table>
+   <table height="10%">
+  	<tr>
+  		<td></td>
+  		<td></td>
+  		<td></td>
+  	</tr>
+  </table>
+<table height="20%">
+  	<tr>
+  		<td></td>
+  		<td></td>
+  		<td></td>
+  	</tr>
+  </table>
+</page>
+<!-- <page size="A4" layout="portrait"></page>
+<page size="A5"></page>
+<page size="A5" layout="portrait"></page>
+<page size="A3"></page>
+<page size="A3" layout="portrait"></page> -->
+  
+  </body>
+</html>
